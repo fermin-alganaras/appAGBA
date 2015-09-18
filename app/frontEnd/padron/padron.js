d86ec9f45476d24af6756eb66e601a840a6eccb8
fermin.alganaras <fermin.alganaras@gmail.com>
2015-09-18 18:20:52 -0300

    ya estan separados los templates por nombre falta definir el aspecto general de la pagina

--- app/frontEnd/padron/padron.js
+++ app/frontEnd/padron/padron.js
@@ -1,10 +1,27 @@
-(function(){
+(function() {
   'use strict';
 
   angular
-  .module('padron', [
-    'padron.delegados',
-    'padron.patinadores',
-    'padron.tecnicos'
-  ])
+    .module('padron', [
+      'padron.delegados',
+      'padron.patinadores',
+      'padron.tecnicos'
+    ])
+
+  .config(function config($stateProvider) {
+    $stateProvider
+      .state('padron', {
+          url: '',
+          views: {
+            'padron@': {
+              templateUrl: 'app/frontEnd/templates/padron-tmpl.html',
+              controller: 'padronController'
+            },
+            'accionesPadron@': {
+              templateUrl: 'app/frontEnd/templates/leftColumn.html',
+              controller: 'padronController'
+            }
+          }
+      })
+  })
 })();
